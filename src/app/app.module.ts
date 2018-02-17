01d63780bdfda0fd75204d42efc931161c704462
ayallum <adam.yallum@gmail.com>
2018-02-17 18:43:25 -0500

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { AddItemPage } from '../pages/add-item/add-item';
+import { ItemDetailPage } from '../pages/item-detail/item-detail';
+import { TabsPage } from '../pages/tabs/tabs';
+import { Data } from '../providers/data';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    AddItemPage,
+    TabsPage,
+    ItemDetailPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    AddItemPage,
+    TabsPage,
+    ItemDetailPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    Data
   ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/pages/add-item/add-item.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NavController, ViewController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-add-item',
+  templateUrl: 'add-item.html'
+})
+export class AddItemPage {
+
+  title;
+  description;
+
+  constructor(public navCtrl: NavController, public view: ViewController) {
+
+  }
+
+  saveItem() {
+
+    let newItem = {
+      name: this.name,
+      price: this.price,
+      category: this.category,
+      photo: this.photo,
+      description: this.description
+    };
+
+    this.view.dismiss(newItem);
+
+  }
+
+  close() {
+    this.view.dismiss();
+  }
+
+}
